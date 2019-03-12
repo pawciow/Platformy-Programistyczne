6ec143256e1fda82f300f83f4436a1f639810750
Paweł Więcław <pawciow123@gmail.com>
2019-03-12 22:44:36 +0100

    Przygotowanie na pierwsze laby
    W sumie tego wyjątku nie trzeba było zrobić, ale on zdecydowanie nie przeszkadza :)

--- Lab01/MainWindow.xaml.cs
+++ Lab01/MainWindow.xaml.cs
@@ -14,18 +14,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Lab01
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary>d
     public partial class MainWindow : Window
     {
+        BitmapImage _Image = new BitmapImage { };
         ObservableCollection<Person> people = new ObservableCollection<Person>
         {
-            new Person { Name = "P1", Age = 1 },
-            new Person { Name = "P2", Age = 2 }
+            new Person { Name = "P1", Age = 1, Picture = new BitmapImage(new Uri("C:\\Users\\Pawel\\Desktop\\dotNet\\Lab01\\Properties\\lena.bmp"))}, // Zmien sobie na jakis swoj obraz u siebie:P
+            new Person { Name = "P2", Age = 2, Picture =  new BitmapImage(new Uri("C:\\Users\\Pawel\\Desktop\\dotNet\\Lab01\\Properties\\lena.bmp"))}
         };
 
         public ObservableCollection<Person> Items
@@ -41,7 +43,31 @@ namespace Lab01
         
         private void AddNewPersonButton_Click(object sender, RoutedEventArgs e)
         {
-            people.Add(new Person { Age = int.Parse(ageTextBox.Text), Name = nameTextBox.Text });
+            try
+            {
+                people.Add(new Person { Age = int.Parse(ageTextBox.Text), Name = nameTextBox.Text, Picture = _Image });
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Nieprawidłowy format w tabelce wiek");
+            }
+            
+        }
+
+        void OnImageButtonClick(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".png";
+            fileDialog.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+            if ((bool)fileDialog.ShowDialog())
+            {
+                string fileName = fileDialog.FileName;
+
+                PictureBox.Source = new BitmapImage(new Uri(fileName));
+                _Image = new BitmapImage(new Uri(fileName));
+            }
+
         }
     }
 }
