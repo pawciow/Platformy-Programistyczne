a1a3cf9276f4ca504f4f5d4773062fdeaca4e393
Kamilka <kamila.rolla@gmail.com>
2019-04-03 23:57:31 +0200

    API

---
+++ Lab01/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Weather
+    {
+        public string City { get; set; }
+        public float  Temperature { get; set; }
+        public string Wind { get; set; }
+    }
+}
